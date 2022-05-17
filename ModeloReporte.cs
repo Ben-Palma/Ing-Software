cdc531fdd2b340b01c2c4e34e735e024575eb9dc
Ben-Palma <68388016+Ben-Palma@users.noreply.github.com>
2022-05-17 14:24:51 -0600

    Se agrega ModeloReporte
    Contiene get y set del ModeloReporte

---
+++ ModeloReporte.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SCyC_Web.ING_SOFTWARE
+{
+    public class ModeloReporte
+    {
+        public string pdfReportPath { get; set; }
+    }
+
+    public class ModeloXML
+    {
+        public string contenidoXml { get; set; }
+    }
+}
